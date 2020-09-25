8806e795c602730fdc3f62ede9cb2c1bed38bb70
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-24 21:15:12 -0500

    Renamed file to server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Static assets 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+//Routes
+app.use(routes);
+
+//Connect to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/______"); //missing database name
+
+
+//Start server
+app.listen(PORT, () => {
+    console.log(`Server now listening on PORT ${PORT}`);
+})
