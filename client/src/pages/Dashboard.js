2aaf672d23661e1a4d9bee62c18256c93fb9fe8f
Evan Boswood <evan@boswood.net>
2020-09-28 21:31:33 -0500

    moved projects array out added routing

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -27,113 +27,6 @@ import {
 	createMuiTheme,
 	useMediaQuery,
 } from "@material-ui/core";
-import TeamOverview from "../components/TeamOverview";
-
-const projectsArray = [
-	{
-		id: 0,
-		title: "Project 1",
-		teams: [
-			{
-				title: "Team 1",
-				content:
-					"Team 1 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-			{
-				title: "Team 2",
-				content:
-					"Team 2 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-			{
-				title: "Team 3",
-				content:
-					"Team 3 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-			{
-				title: "Team 4",
-				content:
-					"Team 4 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-		],
-	},
-	{
-		id: 1,
-		title: "Project 2",
-		teams: [
-			{
-				title: "Team 5",
-				project: 1,
-				content:
-					"Team 5 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-			{
-				title: "Team 6",
-				project: 1,
-				content:
-					"Team 6 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-			{
-				title: "Team 7",
-				project: 1,
-				content:
-					"Team 7 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-		],
-	},
-	{
-		id: 2,
-		title: "Project 3",
-		teams: [
-			{
-				title: "Team 8",
-				project: 2,
-				content:
-					"Team 8 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-			{
-				title: "Team 9",
-				project: 2,
-				content:
-					"Team 9 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-			{
-				title: "Team 10",
-				project: 2,
-				content:
-					"Team 10 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-		],
-	},
-	{
-		id: 3,
-		title: "Project 4",
-		teams: [
-			{
-				title: "Team 11",
-				project: 3,
-				content:
-					"Team 11 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-			{
-				title: "Team 12",
-				project: 3,
-				content:
-					"Team 12 content\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			},
-		],
-	},
-	{
-		id: 4,
-		title: 'Project 5',
-		teams: [
-			{
-				title: "Team 13",
-				project: 4,
-				content: "Team 13 content\nA bunch of bullshit lorem ipsum"
-			}
-		]
-	}
-];
 
 const drawerWidth = 240;
 
@@ -203,6 +96,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function MiniDrawer() {
+
 	const classes = useStyles();
 
 	const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -285,26 +179,26 @@ export default function MiniDrawer() {
 					</div>
 					<Divider />
 					<List>
-						<ListItem button key={"dashboard"}>
-							<ListItemIcon>
+						<ListItem button key={"dashboard"} component="a" href="/">
+							<ListItemIcon name="teams">
 								<DashboardOutlinedIcon />
 							</ListItemIcon>
-							<ListItemText primary={"Dashboard"} />
+							<ListItemText primary={"Dashboard"}/>
 						</ListItem>
-						<ListItem button key={"supervisor"}>
+						<ListItem button key={"admin"} component="a" href="/admin">
 							<ListItemIcon>
 								<SupervisorAccountOutlinedIcon />
 							</ListItemIcon>
 							<ListItemText primary={"Admin"} />
 						</ListItem>
 						<Divider />
-						<ListItem button key={"allmail"}>
+						<ListItem button key={"allmail"} component="a" href="/mail">
 							<ListItemIcon>
 								<MailIcon />
 							</ListItemIcon>
 							<ListItemText primary={"All Mail"} />
 						</ListItem>
-						<ListItem button key={"trashmail"}>
+						<ListItem button key={"trashmail"} component="a" href="/trash">
 							<ListItemIcon>
 								<DeleteOutlineOutlinedIcon />
 							</ListItemIcon>
@@ -313,7 +207,7 @@ export default function MiniDrawer() {
 					</List>
 					<Divider />
 					<List>
-						<ListItem button key={"settings"}>
+						<ListItem button key={"settings"} component="a" href="/settings">
 							<ListItemIcon>
 								<SettingsOutlinedIcon />
 							</ListItemIcon>
@@ -323,7 +217,6 @@ export default function MiniDrawer() {
 				</Drawer>
 				<main className={classes.content}>
 					<div className={classes.toolbar} />
-					<TeamOverview projects={projectsArray}/>
 				</main>
 			</div>
 		</ThemeProvider>
