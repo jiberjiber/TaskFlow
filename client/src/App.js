9ab9b0bf3fb5faf51c767531a9cf63d770807f4c
Evan Boswood <evan@boswood.net>
2020-09-28 21:29:34 -0500

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 // eslint-disable-next-line
-import SignIn from './pages/SignIn';
-import Dashboard from './pages/Dashboard';
-import './App.css';
-import { Container } from '@material-ui/core';
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import "./App.css";
+import TeamOverview from "./pages/TeamOverview";
+import Admin from "./pages/Admin";
+import projectsArray from "./projectsArray";
 
 function App() {
-  return (
-	  <Container>
-		  <Dashboard />
-	  </Container>
-  );
+	return (
+		<BrowserRouter>
+			<Dashboard />
+			<Route
+				exact
+				path="/"
+				render={(props) => <TeamOverview {...props} projects={projectsArray} />}
+			/>
+			<Route
+				exact
+				path="/admin"
+				render={(props) => <Admin {...props} projects={projectsArray} />}
+			/>
+		</BrowserRouter>
+	);
 }
 
 export default App;
