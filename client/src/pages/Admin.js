88834bfc84719f3ec6718ee577a0ce6383569056
Evan Boswood <evan@boswood.net>
2020-09-30 18:43:41 -0500

    added projects table

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 import AddIcon from "@material-ui/icons/Add";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Grid from "@material-ui/core/Grid";
@@ -7,13 +7,15 @@ import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
 import { Divider, Fab } from "@material-ui/core";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -25,33 +27,31 @@ const useStyles = makeStyles((theme) => ({
 	fab: {
 		margin: theme.spacing(2),
 	},
+	m25: {
+		margin: "25px",
+	},
+	m50: {
+		margin: "50px",
+	},
 }));
 
-const columns = [
-	{ id: 'name', label: 'Name', minWidth: 170 },
-	{ id: 'numTeams', label: 'ISO\u00a0Code', minWidth: 100 },
-	{
-	  id: 'population',
-	  label: 'Population',
-	  minWidth: 170,
-	  align: 'right',
-	  format: (value) => value.toLocaleString('en-US'),
+const StyledTableCell = withStyles((theme) => ({
+	head: {
+	  backgroundColor: theme.palette.common.black,
+	  color: theme.palette.common.white,
 	},
-	{
-	  id: 'size',
-	  label: 'Size\u00a0(km\u00b2)',
-	  minWidth: 170,
-	  align: 'right',
-	  format: (value) => value.toLocaleString('en-US'),
+	body: {
+	  fontSize: 14,
 	},
-	{
-	  id: 'density',
-	  label: 'Density',
-	  minWidth: 170,
-	  align: 'right',
-	  format: (value) => value.toFixed(2),
+  }))(TableCell);
+  
+  const StyledTableRow = withStyles((theme) => ({
+	root: {
+	  '&:nth-of-type(odd)': {
+		backgroundColor: theme.palette.action.hover,
+	  },
 	},
-  ];
+  }))(TableRow);
 
 export default function Admin(props) {
 	const classes = useStyles();
@@ -63,7 +63,7 @@ export default function Admin(props) {
 
 	return (
 		<div className={classes.root}>
-			<Grid>
+			<Grid className={classes.m25}>
 				<Grid item xs style={{ textAlign: "center" }}>
 					<Typography variant="h4">New Project</Typography>
 					<Tooltip title="Add">
@@ -74,9 +74,56 @@ export default function Admin(props) {
 				</Grid>
 			</Grid>
 			<Divider />
-			<Table>
-
-			</Table>
+			<Container className={classes.m50}>
+					<Table aria-label="projects-table">
+						<TableHead>
+							<TableRow>
+								<StyledTableCell>
+									<Typography color="textPrimary">Project ID</Typography>
+								</StyledTableCell>
+								<StyledTableCell>
+									<Typography color="textPrimary">Project Name</Typography>
+								</StyledTableCell>
+								<StyledTableCell>
+									<Typography color="textPrimary">Teams</Typography>
+								</StyledTableCell>
+								<StyledTableCell>
+									<Typography color="textPrimary">Due Date</Typography>
+								</StyledTableCell>
+								<StyledTableCell>
+									<Typography color="textPrimary">Actions</Typography>
+								</StyledTableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{props.projects.map((project) => (
+								<TableRow key={project.id}>
+									<StyledTableCell>
+										<Typography color="textPrimary">{project.id}</Typography>
+									</StyledTableCell>
+									<StyledTableCell>
+										<Typography color="textPrimary">{project.title}</Typography>
+									</StyledTableCell>
+									<StyledTableCell>
+										<Typography color="textPrimary">
+											{project.teams.map((team) => team.title + ", ")}
+										</Typography>
+									</StyledTableCell>
+									<StyledTableCell>
+										<Typography color="textPrimary">Jan 1, 1970</Typography>
+									</StyledTableCell>
+									<StyledTableCell>
+										<Tooltip title="Delete">
+											<IconButton>
+												<DeleteIcon color="secondary" />
+											</IconButton>
+										</Tooltip>
+									</StyledTableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+			</Container>
 		</div>
 	);
 }
