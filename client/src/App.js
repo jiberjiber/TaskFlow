6ebc68bd5e8ced4d0e68f6f8df31c204b7a27509
Evan Boswood <evan@boswood.net>
2020-09-30 19:57:34 -0500

    corrected paths

--- client/src/App.js
+++ client/src/App.js
@@ -6,27 +6,27 @@ import Dashboard from "./frames/Dashboard";
 import "./App.css";
 import TeamOverview from "./pages/TeamOverview";
 import Admin from "./pages/Admin";
+import ProjectInfo from "./pages/ProjectInfo";
 import projectsArray from "./projectsArray";
+
+const signedIn = false;
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Dashboard />
-			<Route 
-				exact
-				path="/"
-				render={(props) => <SignIn {...props} />}
-			/>
-			<Route
-				exact
-				path="/home"
-				render={(props) => <TeamOverview {...props} projects={projectsArray} />}
-			/>
-			<Route
-				exact
-				path="/admin"
-				render={(props) => <Admin {...props} projects={projectsArray} />}
-			/>
+			<Route exact path="/" render={(props) => <SignIn {...props} />} />
+			<Route exact path="/home">
+				<Dashboard />
+				<TeamOverview projects={projectsArray} />
+			</Route>
+			<Route exact path="/admin">
+				<Dashboard />
+				<Admin projects={projectsArray} />
+			</Route>
+			<Route exact path="/project/:id">
+				<Dashboard />
+				<ProjectInfo />
+			</Route>
 		</BrowserRouter>
 	);
 }
