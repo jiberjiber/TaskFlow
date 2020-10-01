718f43f200753be6ed0a688b592384ad530ac28b
Evan Boswood <evan@boswood.net>
2020-09-30 19:57:58 -0500

    added project settings and launch icons

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -16,6 +16,8 @@ import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
+import SettingsIcon from '@material-ui/icons/Settings';
+import LaunchIcon from '@material-ui/icons/Launch';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -33,25 +35,28 @@ const useStyles = makeStyles((theme) => ({
 	m50: {
 		margin: "50px",
 	},
+	mt25: {
+		marginTop: "25px",
+	},
 }));
 
 const StyledTableCell = withStyles((theme) => ({
 	head: {
-	  backgroundColor: theme.palette.common.black,
-	  color: theme.palette.common.white,
+		backgroundColor: theme.palette.common.black,
+		color: theme.palette.common.white,
 	},
 	body: {
-	  fontSize: 14,
+		fontSize: 14,
 	},
-  }))(TableCell);
-  
-  const StyledTableRow = withStyles((theme) => ({
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
 	root: {
-	  '&:nth-of-type(odd)': {
-		backgroundColor: theme.palette.action.hover,
-	  },
+		"&:nth-of-type(odd)": {
+			backgroundColor: theme.palette.action.hover,
+		},
 	},
-  }))(TableRow);
+}))(TableRow);
 
 export default function Admin(props) {
 	const classes = useStyles();
@@ -74,55 +79,65 @@ export default function Admin(props) {
 				</Grid>
 			</Grid>
 			<Divider />
-			<Container className={classes.m50}>
-					<Table aria-label="projects-table">
-						<TableHead>
-							<TableRow>
+			<Container className={classes.mt25}>
+				<Table aria-label="projects-table">
+					<TableHead>
+						<TableRow>
+							<StyledTableCell>
+								<Typography color="textPrimary">Project ID</Typography>
+							</StyledTableCell>
+							<StyledTableCell>
+								<Typography color="textPrimary">Project Name</Typography>
+							</StyledTableCell>
+							<StyledTableCell>
+								<Typography color="textPrimary">Teams</Typography>
+							</StyledTableCell>
+							<StyledTableCell>
+								<Typography color="textPrimary">Due Date</Typography>
+							</StyledTableCell>
+							<StyledTableCell>
+								<Typography color="textPrimary">Actions</Typography>
+							</StyledTableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{props.projects.map((project) => (
+							<TableRow key={project.id}>
 								<StyledTableCell>
-									<Typography color="textPrimary">Project ID</Typography>
+									<Typography color="textPrimary">{project.id}</Typography>
 								</StyledTableCell>
 								<StyledTableCell>
-									<Typography color="textPrimary">Project Name</Typography>
+									<Typography color="textPrimary">{project.title}</Typography>
 								</StyledTableCell>
 								<StyledTableCell>
-									<Typography color="textPrimary">Teams</Typography>
+									<Typography color="textPrimary">
+										{project.teams.map((team) => team.title + ", ")}
+									</Typography>
 								</StyledTableCell>
 								<StyledTableCell>
-									<Typography color="textPrimary">Due Date</Typography>
+									<Typography color="textPrimary">Jan 1, 1970</Typography>
 								</StyledTableCell>
 								<StyledTableCell>
-									<Typography color="textPrimary">Actions</Typography>
+									<Tooltip title="Open Project" component="a" href={"/project/" + project.id}>
+										<IconButton>
+											<LaunchIcon color="action" />
+										</IconButton>
+									</Tooltip>
+									<Tooltip title="Manage" component="a" href={"/project/" + project.id + "/manage"}>
+										<IconButton>
+											<SettingsIcon color="action" />
+										</IconButton>
+									</Tooltip>
+									<Tooltip title="Delete" component="a" href={"/project/" + project.id + "/delete"}>
+										<IconButton>
+											<DeleteIcon color="secondary" />
+										</IconButton>
+									</Tooltip>
 								</StyledTableCell>
 							</TableRow>
-						</TableHead>
-						<TableBody>
-							{props.projects.map((project) => (
-								<TableRow key={project.id}>
-									<StyledTableCell>
-										<Typography color="textPrimary">{project.id}</Typography>
-									</StyledTableCell>
-									<StyledTableCell>
-										<Typography color="textPrimary">{project.title}</Typography>
-									</StyledTableCell>
-									<StyledTableCell>
-										<Typography color="textPrimary">
-											{project.teams.map((team) => team.title + ", ")}
-										</Typography>
-									</StyledTableCell>
-									<StyledTableCell>
-										<Typography color="textPrimary">Jan 1, 1970</Typography>
-									</StyledTableCell>
-									<StyledTableCell>
-										<Tooltip title="Delete">
-											<IconButton>
-												<DeleteIcon color="secondary" />
-											</IconButton>
-										</Tooltip>
-									</StyledTableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
+						))}
+					</TableBody>
+				</Table>
 			</Container>
 		</div>
 	);
