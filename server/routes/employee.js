613749a5a59234a0c43c840e03ef683e8f5b58a5
rad-a <11688089+rad-a@users.noreply.github.com>
2020-10-03 10:18:33 -0500

    Added auth middleware and removed pw from send()

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const express = require("express");
@@ -9,8 +9,8 @@ const {
   validateSignupData,
   validate,
 } = require("../middleware/signupValidate");
-const auth=require('../middleware/auth')
-const manager=require('../middleware/managerAuth')
+const auth = require("../middleware/auth");
+const manager = require("../middleware/managerAuth");
 const isManager = require("../middleware/managerAuth");
 const { Project } = require("../models/project.js");
 const { Scope } = require("../models/scope.js");
@@ -31,21 +31,32 @@ router.post("/register", validateSignupData(), validate, async (req, res) => {
         username: data.username,
         isManager: data.isManager,
         email: data.email,
+        password: data.password,
         company: data.company,
       });
 
+      let employeeId = await employee.returnid();
       let pass = employee.returnPassword();
       const salt = await bcrypt.genSalt(10);
       employee.password = await bcrypt.hash(pass, salt);
 
       await employee.save();
+
+      const employeeCompany = await Company.findByIdAndUpdate(
+        { _id: req.body.company },
+        { $push: { employees: employeeId } },
+        { new: true }
+      );
+      // console.log(`USERNAME: ${employee.username}`);
+
+      await employeeCompany.save();
       res.send({
         firstName: employee.firstName,
         lastName: employee.lastName,
         email: employee.email,
         _id: employee._id,
-        password: employee.password,
-        isManager: employee.isManager
+        isManager: employee.isManager,
+        company: data.company,
       });
     }
   } catch (err) {
@@ -54,10 +65,10 @@ router.post("/register", validateSignupData(), validate, async (req, res) => {
   }
 });
 
-//User login 
-router.post("/login", async (req, res) => {
+//User login
+router.post("/login", [auth,manager], async (req, res) => {
   const checkUser = await Employee.findOne({ email: req.body.email });
-  console.log(req.body)
+  console.log(req.body);
   if (!checkUser) return res.status(400).send("iuser not registered ");
 
   const validPassword = await bcrypt.compare(
@@ -69,11 +80,11 @@ router.post("/login", async (req, res) => {
   const token = await checkUser.generateToken();
 
   res.send(token);
-  console.log(token)
+  console.log(token);
 });
 
 //Get all users
-router.get("/", async (req, res) => {
+router.get("/", [auth,manager], async (req, res) => {
   const employees = await Employee.find().select().sort("lastName");
 
   if (!employees) return res.status(400).send("No employees found.");
@@ -86,7 +97,6 @@ router.get("/", async (req, res) => {
       employee.lastName,
       employee.username,
       employee.isManager,
-      employee.password,
       employee.email,
       employee.projectsCreated,
       employee.projectsAssigned,
@@ -98,73 +108,13 @@ router.get("/", async (req, res) => {
 });
 
 //Get user by id
-router.get("/:id", async (req, res) => {
-  const getOneEmployee = await Employee.findById(req.params.id).select();
+router.get("/:id", [auth,manager], async (req, res) => {
+  const getOneEmployee = await Employee.find({ _id: req.params.id }).select();
 
-  if(!getOneEmployee) return res.status(400).send("The employee with this ID does not exist.");
+  if (!getOneEmployee)
+    return res.status(400).send("The employee with this ID does not exist.");
 
   res.send(getOneEmployee);
-});
-
-//Delete a user
-router.delete("/delete/:id", async (req, res, next) => {
-  const user = await Employee.findById(req.params.id).select("_id");
-//TODO: connect team with project
-  if(!user) return res.status(400).send("This user does not exist.");
-// console.log("manager" + req.params.isManager)
-  //TODO: if(user.isManager) {
-    await Project.find( {authorId: user.id}, async (error, projectMatch) => {
-      if(error) return res.status(400).send(error)
-      console.log("Projects FOUND:" + projectMatch) 
-
-      const projectId = projectMatch._id;
-
-      await Scope.deleteMany({ parentProject: projectId}, err => {
-        if(err) return res.status(400).send("Could not delete projects");
-        console.log("Projects deleted")
-      })
-
-    });
-
-    
-  // }
-  //TODO:find tasks and delete tasks
-
-
-  //TODO: find scopes and delete scopes 
-
-//TODO:find prokects
-
-
-//TODO:delete projects
-
-
-//TODO: find and delete empl
-
-
-
-
-
-  // await Company.find({ admin: employee._id }, (company) => {
-  //   if (!employee.isManager) {
-  //     Employee.findByIdAndRemove;
-  //   }
-  // });
-  // const userId = employee._id;
-  const id = req.body;
-console.log("huh" + getEmployee)
-//   try {
-//   const deleteUser = await Employee.findOneAndRemove({ _id: id }).exec((err, removed) => {
-//     Company.findOneAndUpdate({ email: email }, 
-//       { $pull: { employees: id }}, 
-//       { new: true }, 
-//       )
-//   })
-// }catch(err)
-// {
-//   console.log(err)
-//   res.status(400).send(err)
-// }
 });
 
 
