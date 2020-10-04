bc73cc6bf24083b7fb67571ba565278236d73371
jiberjiber <evan@boswood.net>
2020-10-04 16:24:27 -0500

    added user handling and decoding

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Axios from 'axios';
+import jwtDecode from 'jwt-decode';
 import { BrowserRouter, Route } from "react-router-dom";
 // eslint-disable-next-line
 import SignIn from "./pages/SignIn";
@@ -9,11 +11,45 @@ import Admin from "./pages/Admin";
 import ProjectInfo from "./pages/ProjectInfo";
 import projectsArray from "./projectsArray";
 
+
 function App() {
+
+	 const decodeToken = async ()=>{
+		//try catch to prevent app from crashing if there is not token saved
+	   try {
+		const jwt= localStorage.getItem('token');
+		const {firstName, lastName, email, isManager, company, _id}=jwtDecode(jwt);
+
+		console.log('first', firstName, 'last', lastName, 'email', email, 'manager', isManager, 'company', company, 'id', _id);
+	   } catch (error) {
+		   //if error reroute to login page
+		   //could use the same practice:
+		   window.location='/login'
+		  // return null
+	
+	   }
+	
+	}
+
+	console.log(window.location.pathname);
+	const getToken = () => {
+		return localStorage.getItem('token');
+	}
+
+	useEffect(() => {
+		// Get current user token from localstorage
+		Axios.defaults.headers.common['x-auth-token']= getToken();
+
+		// Get user  (decode token)
+		if(window.location.pathname !== '/login'){
+			decodeToken();
+		}
+	},[]);
+
 	return (
 		<BrowserRouter>
-			<Route exact path="/" render={(props) => <SignIn {...props} />} />
-			<Route exact path="/home">
+			<Route exact path="/login" render={(props) => <SignIn {...props} />} />
+			<Route exact path="/">
 				<Dashboard />
 				<TeamOverview projects={projectsArray} />
 			</Route>
