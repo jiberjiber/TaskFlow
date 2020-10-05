bdc9084c560d471ac543e0a13b3353429bfc4557
SabrinaCat <Sabrina.glow22@gmail.com>
2020-10-04 22:34:55 -0500

    need to create html route for create new project page

--- client/src/pages/CreateNew.js
+++ client/src/pages/CreateNew.js
@@ -1,3 +1,8 @@
+//this page will render the layout for creating a
+//new project, it will render the progress bar (stepper ui)
+//the progress bar will render the 3 forms needed
+//to create a new project
+
 import { Box, Button, Card, CardContent, CircularProgress, Grid, Step, StepLabel, Stepper } from '@material-ui/core';
 import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik';
 import { CheckboxWithLabel, TextField } from 'formik-material-ui';
