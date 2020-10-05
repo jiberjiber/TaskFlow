d67b18ccf8f7b85ede6e8976b7bfb8153dd8f328
rad-a <11688089+rad-a@users.noreply.github.com>
2020-10-04 23:31:14 -0500

    Restricted get/  to  user's company. put/ complete

--- server/routes/team.js
+++ server/routes/team.js
@@ -1,48 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
-const { Employee, Company, Team } = require("../models");
-const {
-  validateSignupData,
-  validate,
-} = require("../middleware/signupValidate");
+const { Company, Team } = require("../models");
 const auth = require("../middleware/auth");
 const manager = require("../middleware/managerAuth");
-const isManager = require("../middleware/managerAuth");
-const { Project } = require("../models/project.js");
-const { Scope } = require("../models/scope.js");
+const {validateTeamData, validate} = require("../middleware/teamValidation");
 
-router.post("/add", [auth, manager], async (req, res) => {
+router.post("/add", [auth, manager], validateTeamData(), validate, async (req, res) => {
   try {
     const data = req.body;
     const { _id, firstName, lastName } = req.employee;
-
+    const company = await Company.findOne({ employees: { $in: _id } }).select();
     const newTeam = new Team({
       name: data.name,
       owner: `${firstName} ${lastName}`,
       ownerId: _id,
       members: data.members,
       assignedProjects: data.assignedProjects,
+      company: company._id,
     });
-
     await newTeam.save();
     console.log(newTeam);
     res.send(newTeam);
   } catch (err) {
     res.status(400);
-    return res.send("Team could not be created.");
+    return res.send(err.message);
   }
 });
 
-router.get("/", async (req, res) => {
-  const allTeams = await Team.find({})
+router.get("/", [auth], async (req, res) => {
+  const { _id } = req.employee;
+  const allTeams = await Team.find({
+    $or: [{ ownerId: { $in: _id } }, { members: { $in: _id } }],
+  })
+    .select()
     .populate("members")
-    .populate("assignedProjects");
+    .populate("assignedScope");
+
   if (!allTeams) return res.status(400).json({ teams: "No teams to display." });
   res.send(allTeams);
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", [auth], async (req, res) => {
   const oneTeam = await Team.find({ _id: req.params.id })
     .populate("members")
     .select();
@@ -57,25 +56,29 @@ router.get("/:id", async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
     const teamId = req.params.id;
-    // const { _id, firstName, lastName } = req.employee;
     const newData = req.body.members;
-
+    const getMember = await Team.findOne({members:{$in: newData}}).select();
+    if (getMember) return res.status(400).send("One or more user is already in the team");
     if (!teamId) return res.status(400).send("Please provide a valid team Id");
-    const updateTeam = await Team.findByIdAndUpdate(
-      { _id: teamId },
-      { $push: { members: newData } },
-      { safe: true, upsert: true, new: true }
-    );
-
-    res.send(updateTeam);
+    if (newData.length == 0)
+      return res.status(400).send("No members to display.");
+    newData.map(async (info) => {
+      await Team.findByIdAndUpdate(
+        { _id: teamId },
+        { $push: { members: info } },
+        { safe: true, upsert: true, new: true }
+      );
+      const newTeam = await Team.findById(teamId)
+        .populate("members")
+        .select()
+        .sort("firstName");
+      res.send(newTeam);
+    });
   } catch (err) {
     console.log(err);
     res.status(400).send(err);
   }
 });
 
-// router.delete("/:id", async (req, res) => {
-
-// });
 
 module.exports = router;
