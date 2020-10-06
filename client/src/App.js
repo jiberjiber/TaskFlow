7fcdbf762fc4885dbe1c54745564cfe3b9398451
Evan Boswood <evan@boswood.net>
2020-10-05 21:04:48 -0500

    added usertype specific routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,57 @@
 import React, { useState, useEffect } from "react";
-import Axios from 'axios';
-import jwtDecode from 'jwt-decode';
+import Axios from "axios";
+import jwtDecode from "jwt-decode";
 import { BrowserRouter, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./frames/Dashboard";
-import "./App.css";
 import TeamOverview from "./pages/TeamOverview";
 import Admin from "./pages/Admin";
 import ProjectInfo from "./pages/ProjectInfo";
-import projectsArray from './projectsArray';
-
+import projectsArray from "./projectsArray";
+import Error from './pages/Error';
+import EmployeeOverview from './pages/EmployeeOverview';
+import "./App.css";
 
 function App() {
-
 	const [user, setUser] = useState({});
 	const [projects, setProjects] = useState([]);
 
 	const decodeToken = async () => {
 		//try catch to prevent app from crashing if there is not token saved
 		try {
-			const jwt = localStorage.getItem('token');
+			const jwt = localStorage.getItem("token");
 			setUser(jwtDecode(jwt));
 			getProjects();
-
 		} catch (error) {
 			//if error reroute to login page
 			//could use the same practice:
-			window.location = '/login'
+			window.location = "/login";
 			// return null
-
 		}
-
-	}
+	};
 
 	const getToken = () => {
-		return localStorage.getItem('token');
-	}
+		return localStorage.getItem("token");
+	};
 
 	useEffect(() => {
 		// Get current user token from localstorage
-		Axios.defaults.headers.common['x-auth-token'] = getToken();
+		Axios.defaults.headers.common["x-auth-token"] = getToken();
 
 		// Get user  (decode token)
-		if (window.location.pathname !== '/login') {
+		if (window.location.pathname !== "/login") {
 			decodeToken();
 		}
+	// eslint-disable-next-line
 	}, []);
 
-	const getProjects = () => {
-		Axios.get('/api/project')
+	function getProjects() {
+			Axios.get("/api/project")
 			.then((response) => {
 				setProjects(response.data);
+				console.log(response.data);
 			})
-			.catch(err => {
+			.catch((err) => {
 				console.log(err);
 			});
 	}
@@ -61,12 +60,32 @@ function App() {
 		<BrowserRouter>
 			<Route exact path="/login" render={(props) => <SignIn {...props} />} />
 			<Route exact path="/">
-				<Dashboard user={user} />
-				<TeamOverview projects={projects} />
+				{user.isManager && (
+					<div>
+						<Dashboard user={user} />
+						<TeamOverview projects={projects} />
+					</div>
+				)}
+				{!user.isManager && (
+					<div>
+						<Dashboard user={user} />
+						<EmployeeOverview projects={projects} user={user} />
+					</div>
+				)}
 			</Route>
 			<Route exact path="/admin">
-				<Dashboard user={user} />
-				<Admin projects={projectsArray} />
+				{user.isManager && (
+					<div>
+						<Dashboard user={user} />
+						<Admin projects={projectsArray} />
+					</div>
+				)}
+				{!user.isManager && (
+					<div>
+						<Dashboard user={user} />
+						<Error />
+					</div>
+				)}
 			</Route>
 			<Route exact path="/project/:id">
 				<Dashboard user={user} />
