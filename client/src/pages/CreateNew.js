59efff995d59dddc0bc24a32a94b96f9af5f8acc
Evan Boswood <evan@boswood.net>
2020-10-05 21:44:40 -0500

    added container for formatting

--- client/src/pages/CreateNew.js
+++ client/src/pages/CreateNew.js
@@ -3,54 +3,89 @@
 //the progress bar will render the 3 forms needed
 //to create a new project
 
-import { Box, Button, Card, CardContent, CircularProgress, Grid, Step, StepLabel, Stepper } from '@material-ui/core';
-import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik';
-import { CheckboxWithLabel, TextField } from 'formik-material-ui';
-import React, { useState } from 'react';
-import { mixed, number, object } from 'yup';
+import {
+	Box,
+	Button,
+	Card,
+	CardContent,
+	CircularProgress,
+	Grid,
+	Step,
+	StepLabel,
+	Stepper,
+	Container,
+} from "@material-ui/core";
+import { Field, Form, Formik, FormikConfig, FormikValues } from "formik";
+import { CheckboxWithLabel, TextField } from "formik-material-ui";
+import React, { useState } from "react";
+import { mixed, number, object } from "yup";
 
 const sleep = (time) => new Promise((acc) => setTimeout(acc, time));
 
 export default function CreateNewProject() {
-    return (
-        <Card>
-            <CardContent>
-                <Formik
-                    initialValues={{
-                        //project start values
-                        title: '',
-                        description: '',
-                        dueDate: '',
-                        //task start values
-                        scopeName: '',
-                        dueDate: '',
-                    }}
-                    //waits for promise to resolve or reject
-                    //then logs values
-                    onSubmit={async (values) => {
-                        await sleep(3000);
-                        console.log('values', values);
-                    }}>
-                
-                    <Form label="Create A New Project">
-                        <Box paddingBottom={2}>
-                            <Field fullWidth variant="outlined" name="title" component={TextField} label="Title of Project" />
-                        </Box>
-                        <Box paddingBottom={2}>
-                            <Field fullWidth variant="outlined" name="description" component={TextField} label="Description of Main Objective" />
-                        </Box>
-                        <Box paddingBottom={2}>
-                            <Field fullWidth variant="outlined" name="dueDate" component={TextField} label='What is the completion date for this project?' />
-                        </Box>
-                    </Form>
-                </Formik>
-            </CardContent>
-        </Card>
-                    
-    )} 
-    export function FormikStepper({ children, ...props}: FormikConfig<FormikValues>){
-        return (
-            <Formik {...props}>
-                <Form autoComplete="off">{children}</Form>
-            </Formik>
-        )}
+	return (
+		<Container>
+			<Card>
+				<CardContent>
+					<Formik
+						initialValues={{
+							//project start values
+							title: "",
+							description: "",
+							dueDate: "",
+							//task start values
+							scopeName: "",
+							dueDate: "",
+						}}
+						//waits for promise to resolve or reject
+						//then logs values
+						onSubmit={async (values) => {
+							await sleep(3000);
+							console.log("values", values);
+						}}
+					>
+						<Form label="Create A New Project">
+							<Box paddingBottom={2}>
+								<Field
+									fullWidth
+									variant="outlined"
+									name="title"
+									component={TextField}
+									label="Title of Project"
+								/>
+							</Box>
+							<Box paddingBottom={2}>
+								<Field
+									fullWidth
+									variant="outlined"
+									name="description"
+									component={TextField}
+									label="Description of Main Objective"
+								/>
+							</Box>
+							<Box paddingBottom={2}>
+								<Field
+									fullWidth
+									variant="outlined"
+									name="dueDate"
+									component={TextField}
+									label="What is the completion date for this project?"
+								/>
+							</Box>
+						</Form>
+					</Formik>
+				</CardContent>
+			</Card>
+		</Container>
+	);
+}
+export function FormikStepper({
+	children,
+	...props
+}: FormikConfig<FormikValues>) {
+	return (
+		<Formik {...props}>
+			<Form autoComplete="off">{children}</Form>
+		</Formik>
+	);
+}
