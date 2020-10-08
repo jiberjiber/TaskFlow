dc71c93d6e637c01674aee077cd53abfe0d2a95c
Evan Boswood <evan@boswood.net>
2020-10-07 22:03:45 -0500

    updated team card

--- client/src/components/TeamCard.js
+++ client/src/components/TeamCard.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Grid, Card, CardContent, CardActions, Typography, Button } from '@material-ui/core';
 
 export default function TeamCard(props) {
+
+	console.log(props);
 	return (
 		<Grid item xs={4}>
 			<Card>
 				<CardContent>
 					<Typography variant="h3" gutterBottom>
-						{props.title}
+						{props.scope.scopeName}
 					</Typography>
-					<Typography variant="caption">Due Date: {props.content}</Typography>
+					<Typography variant="caption">Due Date: {props.scope.dueDate}</Typography>
 				</CardContent>
 				<CardActions>
-					<Button variant="contained" size="small">View Team</Button>
+					<Button variant="contained" size="small" component="a" href={`/scope/${props.scope._id}`}>View Tasks</Button>
 				</CardActions>
 			</Card>
 		</Grid>
