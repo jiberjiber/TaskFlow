512e335697b45ab0c22eeef3a44901b40cfa1be8
rad-a <11688089+rad-a@users.noreply.github.com>
2020-10-09 17:46:27 -0500

    Cleaned up file

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -2,149 +2,7 @@
 
 const nodemailer = require("nodemailer");
 const nodemailerSendgrid = require("nodemailer-sendgrid");
-// const jwt = require("jsonwebtoken");
 require("dotenv").config();
-// const { EmployeeSchema } = require("../models/");
-// const appImg = require("../../client/public/app-screenshot.jpg")
-
-// const transport = nodemailer.createTransport(
-//   nodemailerSendgrid({
-//     apiKey: process.env.SENDGRID_API_KEY,
-//   })
-// );
-
-// const sendConfirmationEmail = async (employee) => {
-//   const token = await jwt.sign(
-//     {
-//         _id: employee._id,
-//         firstName: employee.firstName,
-//         lastName: employee.lastName,
-//         username: employee.username,
-//         email: employee.email,
-//         isManager: employee.isManager,
-//         company: employee.company,
-//     },
-//     process.env.SECRET, {expiresIn: "3m"}
-//   );
-//   const url = `${process.env.CLIENT_URL}/confirmation/${token}`;
-
-//   transport
-//     .sendMail({
-//       from: "rt.terabytes@gmail.com",
-//       to: `${employee.firstName} <${employee.email}>`,
-//       subject: "confirmation",
-//       html: `<h1>Hi ${employee.firstName}, please use this link to confirm your accout: <a href=${url}>Confirmation</h1>. This link expires in 3 minutes`,
-//     })
-//     .then(() => {
-//       console.log("Email sent!");
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
-// //TODO: confirm account
-// const confirmAccount = (req, res) => {
-//   const {token} = req.body;
-//   if(token) {
-//     jwt.verify(token, process.env.SECRET, function(err, decodedToken) {
-//       if(err) {
-//         return res.status(400).json({error: "Incorrect or expired link"})
-//       }
-//       // const {name, email, password}
-//     })
-//   } else {
-//     return res.json({error: "Something went wrong!"})
-//   }
-// }
-
-// // module.exports = sendConfirmationEmail;
-// exports.sendConfirmationEmail = sendConfirmationEmail;
-
-//===USING JUST NODEMAILER AND GMAIL
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.GMAIL_USER,
-//     pass: process.env.GMAIL_PASS,
-//   },
-// });
-
-// // const mailOptions =
-// const sendConfirmationEmail = async (employee) => {
-//   try {
-//     const emailToken = jwt.sign(
-//       {
-//         _id: employee._id,
-//         firstName: employee.firstName,
-//         lastName: employee.lastName,
-//         username: employee.username,
-//         email: employee.email,
-//         isManager: employee.isManager,
-//         company: employee.company,
-//       },
-//       process.env.SECRET,
-//       { expiresIn: "4m" }
-//     );
-//     const url = `http:localhost:3001/confirmation/${emailToken}`;
-
-//     await transporter.sendMail({
-//       to: `${employee.firstName} <${employee.email}>`,
-//       subject: "confirmation",
-//       html: `<h3>Confirmation email: <a href=${url}>Confirmation</h3>`,
-//     });
-//     console.log("email send!");
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// exports.sendConfirmationEmail = sendConfirmationEmail;
-
-// const transport = nodemailer.createTransport(
-//   nodemailerSendgrid({
-//     apiKey: process.env.SENDGRID_API_KEY,
-//   })
-// );
-
-//TODO: reset pw
-// const sendPwResetEmail = async (req, res) => {
-//   const { _id, email, password, firstName } = await req.employee;
-//   // const userId = await jwt.sign(
-//   //   {
-//   //       _id: employee._id,
-//   //       firstName: employee.firstName,
-//   //       lastName: employee.lastName,
-//   //       username: employee.username,
-//   //       email: employee.email,
-//   //       isManager: employee.isManager,
-//   //       company: employee.company,
-//   //   },
-//   //   process.env.SECRET, {expiresIn: "3m"}
-//   // );
-//   const url = `${process.env.CLIENT_URL}/passwordrecovery/${_id}`;
-
-//   transport
-//     .sendMail({
-//       from: "rt.terabytes@gmail.com",
-//       to: `${firstName} <${email}>`,
-//       subject: "confirmation",
-//       html: `<h1>Hi ${firstName}, please use this link to reset your password: <a href=${url}>Password reset</h1>.
-//       <p>This link expires in 3 minutes</p>`,
-//     })
-//     .then(([res]) => {
-//       console.log("Email sent!");
-//     })
-//     .catch((err) => {
-//       console.log("Errors. Failed to deliver message");
-
-//       if(err.response && err.body && err.response.body.errors) {
-//         err.response.body.errors.forEach(error => console.log(res.statusCode, res,statusMessage));
-//           } else {
-//             console.log(err);
-//           }
-//     });
-// };
 
 //Registration confirmation Email
 const transport = nodemailer.createTransport(
@@ -162,7 +20,7 @@ const sendWelcomeEmail = async (employee) => {
     .sendMail({
       from: "rt.terabytes@gmail.com",
       to: `${firstName} <${email}>`,
-      subject: "Welcome to {APP NAME}",
+      subject: "Welcome to TaskFlow",
       html: `<div class="wrapper" data-link-color="#cccccc" data-body-style="font-size:16px; font-family:verdana,geneva,sans-serif; color:#516775; background-color:#e8f2ff;">
       <div class="webkit">
         <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#f0f5fb">
@@ -191,7 +49,7 @@ const sendWelcomeEmail = async (employee) => {
                                     <tbody>
                                       <tr>
                                         <td style="background-color:#ffffff; padding:50px 0px 10px 0px; line-height:30px; text-align:inherit;" height="100%" valign="top" bgcolor="#ffffff">
-                                          <div style="font-family: inherit; text-align: center"><span style="color: #516775; font-size: 28px; font-family: georgia,serif"><strong>Welcome to {APP NAME}, ${firstName}!</strong></span>
+                                          <div style="font-family: inherit; text-align: center"><span style="color: #516775; font-size: 28px; font-family: georgia,serif"><strong>Welcome to TaskFlow, ${firstName}!</strong></span>
                                           </div>
                                         </td>
                                       </tr>
@@ -201,7 +59,7 @@ const sendWelcomeEmail = async (employee) => {
                                     <tbody>
                                       <tr>
                                         <td style="background-color:#ffffff; padding:10px 40px 50px 40px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="#ffffff">
-                                          <div style="font-family: inherit; text-align: center"><span style="font-family: verdana,geneva,sans-serif">Thanks for signing up for our app! We created {APP} to help managers and business owners manage their team projects efficiently. We're are happy you found us!</span>
+                                          <div style="font-family: inherit; text-align: center"><span style="font-family: verdana,geneva,sans-serif">Thanks for signing up for our app! We created TaskFlow to help managers and business owners manage their team projects efficiently. We're are happy you found us!</span>
                                           </div>
                                         </td>
                                       </tr>
@@ -312,5 +170,4 @@ const sendWelcomeEmail = async (employee) => {
     });
 };
 
-// module.exports = sendConfirmationEmail;
 exports.sendWelcomeEmail = sendWelcomeEmail;
