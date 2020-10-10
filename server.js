f4c98ffc020d9244a2499851c88bacfa5fed4c4d
Evan Boswood <evan@boswood.net>
2020-10-10 12:28:34 -0500

    added catchall route

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const routes = require("./server/routes");
 // const bodyParser = require("body-parser");
-const moment = require('moment');
-const Joi = require('joi');
+const moment = require("moment");
+const Joi = require("joi");
 const compression = require("compression");
-require('dotenv').config();
+require("dotenv").config();
 
 // if(!process.env.SECRET){
 //     console.error('JWT Priavte key not Defined');
@@ -25,31 +25,33 @@ app.use(compression()); // For compressing the response body to increase the spe
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let mongoDataBase=process.env.NODE_ENV; 
-//Static assets 
+let mongoDataBase = process.env.NODE_ENV;
+//Static assets
 if (mongoDataBase === "production") {
-    app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 }
 
 //Routes
 app.use(routes);
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 //Connect to Mongo DB
 //crossing out mongo so we can use our local db when testing
 //process.env.MONGODB_URI
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/management", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-     useUnifiedTopology: true 
-  });
+	useNewUrlParser: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
+});
 
-
- mongoose.connection.on('connected',()=>{
-    console.log('Mongoose connected')
-  })
-  
+mongoose.connection.on("connected", () => {
+	console.log("Mongoose connected");
+});
 
 //Start server
 app.listen(PORT, () => {
-    console.log(`Server now listening on PORT ${PORT}`);
-})
+	console.log(`Server now listening on PORT ${PORT}`);
+});
